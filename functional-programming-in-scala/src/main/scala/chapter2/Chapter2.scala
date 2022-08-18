14ef35c56e5762f78fde8c734ec710231d1cff69
tredontho <tredontho@gmail.com>
2022-08-17 20:07:48 -0500

    Add answers to Chapter 2 exercies

--- functional-programming-in-scala/src/main/scala/chapter2/Chapter2.scala
+++ functional-programming-in-scala/src/main/scala/chapter2/Chapter2.scala
@@ -33,3 +33,32 @@ object Chapter2 {
     (a: A) => f(g(a))
   }
 }
+
+object Answers {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, current: Int, next: Int): Int =
+      if (n == 0) current
+      else go(n - 1, next, current + next)
+
+    go(n, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if (n + 1 >= as.length) true
+      else if (gt(as(n), as(n + 1))) false
+      else loop(n + 1)
+    loop(0)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a,b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+}
