8cd9bc167fca4b0519d0a304eb871bec412df8a4
tredontho <tredontho@gmail.com>
2022-12-05 23:06:11 -0600

    feat(aoc): adds day 4 haskell solutions

---
+++ advent-of-code/2022/haskell/src/Day04/Part01.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TypeApplications #-}
+module Day04.Part01 where
+
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char (digitChar, char)
+
+solve :: FilePath -> IO String
+solve file = do
+  input <- readFile file
+  pure . show $ solution input
+
+solution :: String -> Int
+solution = length . filter (\(x,y) -> x `fullyContains` y || y `fullyContains` x) . map parseLine . lines
+
+type Parser = Parsec Void String
+
+lineParser :: Parser (Range, Range)
+lineParser = do
+  l1 <- some digitChar
+  _ <- char '-'
+  u1 <- some digitChar
+  _ <- char ','
+  l2 <- some digitChar
+  _ <- char '-'
+  u2 <- some digitChar
+  return (Range (read @Int l1) (read @Int u1), Range (read @Int l2) (read @Int u2))
+
+parseLine :: String -> (Range, Range)
+parseLine = either (error . errorBundlePretty) id . parse lineParser ""
+
+data Range = Range
+  { lowerBound :: Int
+  , upperBound :: Int
+  }
+
+-- | `fullyContains x y` returns `True` if all of y is contained in x
+fullyContains :: Range -> Range -> Bool
+fullyContains x y = lowerBound x <= lowerBound y && upperBound x >= upperBound y
---
+++ advent-of-code/2022/haskell/src/Day04/Part02.hs
@@ -0,0 +1,19 @@
+module Day04.Part02 where
+
+import Day04.Part01 (parseLine, Range(..))
+
+solve :: FilePath -> IO String
+solve file = do
+  input <- readFile file
+  pure . show $ solution input
+
+solution :: String -> Int
+solution = length . filter (uncurry overlaps) . map parseLine . lines
+
+-- (a,b) overlaps with (c,d)
+-- 
+-- if a <= d and b >= c
+-- or c <= b and d >= a
+overlaps :: Range -> Range -> Bool
+overlaps x y = (lowerBound x <= upperBound y && upperBound x >= lowerBound y)
+  || (lowerBound y <= upperBound x && upperBound y >= lowerBound x)
