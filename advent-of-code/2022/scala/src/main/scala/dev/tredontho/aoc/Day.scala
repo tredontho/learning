70e1ee97c32ea5f67957440ab7e24e5dc0e74480
tredontho <tredontho@gmail.com>
2022-12-23 17:14:16 -0600

    Make aoc package

---
+++ advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day.scala
@@ -0,0 +1,3 @@
+package dev.tredontho.aoc
+
+// Each Day will consist of some means to parse the input, a method for taking the parsed input and creating a solution for part 2, and a method for taking the parsed input and creating a solution for part 2.
