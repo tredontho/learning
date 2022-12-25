463fb63c947a0aa51bc84426d1e7297aa6491492
tredontho <tredontho@gmail.com>
2022-12-24 20:21:10 -0600

    Add a common Day class and trait... hopefully it's useful, who knows

--- advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day.scala
+++ advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day.scala
@@ -1,5 +1,15 @@
 package dev.tredontho.aoc
 
-// Each Day will consist of some means to parse the input, a method for taking the parsed input and creating a solution for part 2, and a method for taking the parsed input and creating a solution for part 2.
+trait Day[Input, PartOneOutput, PartTwoOutput] {
+  def solution: DaySolution[Input, PartOneOutput, PartTwoOutput]
+}
 
-case class Day[PartOneOutput, PartTwoOutput]()
+// Each Day will consist of some means to parse the input, a method for taking
+// the parsed input and creating a solution for part 1, and a method for taking
+// the parsed input and creating a solution for part 2.
+
+case class DaySolution[Input, PartOneOutput, PartTwoOutput](
+    parser: String => Input,
+    partOneSolver: Input => PartOneOutput,
+    partTwoSolver: Input => PartTwoOutput
+)
