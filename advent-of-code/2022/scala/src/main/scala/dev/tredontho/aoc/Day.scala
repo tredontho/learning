7d74b65c2e243e9e10486dbc64f185e23e7bb2b3
tredontho <tredontho@gmail.com>
2022-12-24 19:48:58 -0600

    STASH

--- advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day.scala
+++ advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day.scala
@@ -1,3 +1,5 @@
 package dev.tredontho.aoc
 
 // Each Day will consist of some means to parse the input, a method for taking the parsed input and creating a solution for part 2, and a method for taking the parsed input and creating a solution for part 2.
+
+case class Day[PartOneOutput, PartTwoOutput]()
--- advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day01.scala
+++ advent-of-code/2022/scala/src/main/scala/dev/tredontho/aoc/Day01.scala
@@ -1,6 +1,5 @@
 package dev.tredontho
 
-import scala.io.Source
 
 object Day01 extends App with Solver {
   override def solve(lines: List[String]): Solution = {
