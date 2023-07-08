526805c9f7f1166f0bd3dd8cbd3bd09a24c97193
tredontho <tredontho@gmail.com>
2023-07-07 21:36:39 -0500

    feat(leetcode): problem 5
    New approaches still exceed memory limit

--- leetcode/scala/src/main/scala/dev/tredontho/leetcode/problem/P0005.scala
+++ leetcode/scala/src/main/scala/dev/tredontho/leetcode/problem/P0005.scala
@@ -10,12 +10,47 @@ object P0005 extends Problem[String, String] {
     "Given a string s, return the longest palindromic substring in s."
 
   override def solve(input: String): String = {
-    val xs = input.toList
-    val palindromes = (for {
-      ys <- xs.tails.filter(_.nonEmpty)
-      zs <- ys.inits.filter(w => w.nonEmpty && palindrome(w).isDefined)
-    } yield (zs))
-    palindromes.maxBy(_.length).mkString
+    val l = input.toList
+    l.tails
+      .filter(_.nonEmpty)
+      .map { xs =>
+        xs.inits
+          .filter(w => w.nonEmpty && palindrome(w).isDefined)
+          .maxBy(_.length)
+      }
+      .maxBy(_.length)
+      .mkString
+  }
+
+  def idk[A](l: List[A]): List[A] = {
+    l.tails
+      .filter(_.nonEmpty)
+      .map { xs =>
+        xs.inits
+          .filter(w => w.nonEmpty && palindrome(w).isDefined)
+          .maxBy(_.length)
+      }
+      .maxBy(_.length)
+  }
+
+  def idk2[A](l: List[A]): List[A] = {
+    l.tails
+      .filter(_.nonEmpty)
+      .map(
+        _.inits
+          .filter(_.nonEmpty)
+          .reduceRight((x, y) =>
+            if (x.length > y.length && palindrome(x).isDefined) x else y
+          )
+      )
+      .toList
+      .maxBy(_.length)
+  }
+
+  def longestPalindrome[A](l: List[List[A]]): List[A] = {
+    l.reduceRight((x, y) =>
+      if (x.length > y.length && palindrome(x).isDefined) x else y
+    )
   }
 
   def palindrome[A](s: List[A]): Option[List[A]] =
