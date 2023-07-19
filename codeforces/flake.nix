f6b31f18c880ba3ea7533d21ff7697ac05fc302e
tredontho <tredontho@gmail.com>
2023-07-19 17:36:42 -0500

    chore(codeforces): Use the HLS version that is built with the corresponding GHC version

--- codeforces/flake.nix
+++ codeforces/flake.nix
@@ -16,6 +16,7 @@
             haskellPackages.cabal-install
             haskellPackages.ghcid
             haskellPackages.ghc
+            haskellPackages.haskell-language-server
             pkgs.haskellPackages.fourmolu # Trying to use the version of fourmolu from haskellPackages fails because it needs cabal version 3.6
           ];
         };
