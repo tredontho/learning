22d4de8d68bbb6140c54b513ba191506f81df201
tredontho <tredontho@gmail.com>
2023-12-04 18:59:42 -0600

    chore(aoc): Add haskell flake for AOC 2023

---
+++ advent-of-code/2023/haskell/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Flake for haskell development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [pkgs.cabal-install pkgs.ghcid pkgs.ghc pkgs.haskellPackages.fourmolu];
+        };
+      });
+}
