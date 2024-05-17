0480472a8c7a766687a603df3f21cbffc9350b8d
tredontho <tredontho@gmail.com>
2024-05-17 17:23:53 -0500

    feat: Setup for learning Rust with exercises

---
+++ rust/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake for working through the Rust Book";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    workshop-runner.url = "./workshop-runner";
+  };
+
+  outputs = { self, nixpkgs, utils, workshop-runner}: 
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rust-analyzer
+            rustc
+            rustfmt
+            workshop-runner.packages.${system}.default
+          ];
+        };
+      });
+}
