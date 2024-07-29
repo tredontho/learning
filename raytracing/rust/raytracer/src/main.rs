05798c6e290d7cfc53a516a89d88a3b4c65a98c8
tredontho <tredontho@gmail.com>
2024-07-28 21:07:01 -0500

    chore: bit of cleanup, match the gradient to the tutorial

--- raytracing/rust/raytracer/src/main.rs
+++ raytracing/rust/raytracer/src/main.rs
@@ -1,10 +1,8 @@
 use raytracer::RGB;
 fn main() {
-    let width = 640;
-    let height = 480;
+    let width = 256;
+    let height = 256;
 
-    eprintln!("Maximum width/height is {}", usize::max_value());
-    // let image = raytracer::PPM::new(width, height, vec![RGB(0,0,0);width*height]);
     let image = raytracer::PPM::gradient(width, height);
     println!("{}", image.to_string());
 }
