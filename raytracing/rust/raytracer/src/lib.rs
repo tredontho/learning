c755cc0b4ef742e407f9c12c7ffe084b9e830212
tredontho <tredontho@gmail.com>
2024-07-28 20:54:40 -0500

    feat: Start on rust raytracer

---
+++ raytracing/rust/raytracer/src/lib.rs
@@ -0,0 +1,58 @@
+pub fn say_hello(name: &str) {
+    println!("Hello {name}");
+}
+
+#[derive(Debug, Clone)]
+pub struct RGB(pub usize, pub usize, pub usize);
+
+#[derive(Debug)]
+pub struct PPM {
+    width: usize,
+    height: usize,
+    data: Vec<RGB>,
+}
+
+impl PPM {
+    pub fn new(width: usize, height: usize, data: Vec<RGB>) -> PPM {
+        assert_eq!(data.len(), width * height);
+        PPM {
+            width,
+            height,
+            data,
+        }
+    }
+
+    pub fn gradient(width: usize, height: usize) -> PPM {
+        let mut data: Vec<RGB> = Vec::with_capacity(width * height);
+        for i in 0..height {
+            for j in 0..width {
+                let r = (255.0 * i as f32 / (height - 1) as f32) as usize;
+                let g = (255.0 * j as f32 / (width - 1) as f32) as usize;
+                let b = 0;
+                data.push(RGB(r, g, b));
+            }
+        }
+        PPM {
+            width,
+            height,
+            data,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut buffer = String::new();
+        buffer.push_str("P3\n");
+        buffer.push_str(&format!("{} {}\n", self.width, self.height));
+        buffer.push_str("255\n");
+        // TODO: can probably factor this out
+        for (i, pixel) in self.data.iter().enumerate() {
+            buffer.push_str(&format!("{} {} {}", pixel.0, pixel.1, pixel.2));
+            if (i + 1) % self.width == 0 {
+                buffer.push('\n');
+            } else if (i + 1) != self.data.len() {
+                buffer.push(' ');
+            }
+        }
+        buffer
+    }
+}
